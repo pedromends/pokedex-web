bd192169b9f287046d383e419cda97331e738e6e
pedro <joaopedromend14@gmail.com>
2022-12-13 20:43:39 -0300

    prototipo finalizado

--- src/views/DeleteView.vue
+++ src/views/DeleteView.vue
@@ -1,9 +1,42 @@
+<!-- eslint-disable max-len -->
 <template>
   <section class="pt-2">
     <p class="pt-1 text-black text-lg">Deletar Pokémon</p>
     <ul class="h-32 mt-1 text-left">
       <li class="mb-3" v-for="(pokemon,i) in pokemonList" :key="i">
-        <a @click="deleteFlow(pokemon.id)" @keypress="deleteFlow(pokemon.id)">{{ pokemon.nome }}</a>
+        <v-dialog
+          v-model="dialog"
+          width="500"
+          >
+          <template v-slot:activator="{ on, attrs }">
+            <a v-bind="attrs" v-on="on" @click="deleteFlow(pokemon.id)" @keypress="deleteFlow(pokemon.id)">
+              {{ pokemon.nome }}
+            </a>
+          </template>
+
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              Privacy Policy
+            </v-card-title>
+
+            <v-card-text>
+              Deletado com sucesso
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="flowToHome"
+                >
+                Ok
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </li>
     </ul>
   </section>
@@ -17,6 +50,7 @@ export default {
   data() {
     return {
       pokemonList: undefined,
+      dialog: false,
     };
   },
   created() {
@@ -28,12 +62,16 @@ export default {
   },
   methods: {
     deleteFlow(id) {
-    // exibir pop-up
+      // exibir pop-up
       deletePokemon((id)).then((response) => {
         console.log(JSON.stringify(response.data));
       }).catch((error) => {
         console.log(error);
       });
+    },
+    flowToHome() {
+      this.dialog = false;
+      this.$router.push('/');
     },
   },
 };
